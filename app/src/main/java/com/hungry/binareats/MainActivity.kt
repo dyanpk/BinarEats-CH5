85ad5d2e2e320e8167743aa61fad872ad2117e58
Dian Permata Kusuma <diankusuma664@gmail.com>
2023-10-21 22:55:27 +0700

    Initial commit

---
+++ app/src/main/java/com/hungry/binareats/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.hungry.binareats
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.hungry.binareats.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        setBottomNavigation()
+    }
+
+    private fun setBottomNavigation() {
+        val navView: BottomNavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        navView.setupWithNavController(navController)
+    }
+}
